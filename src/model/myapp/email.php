8d1d2d6bfd89e8eb631579254f96d3a985ad6385
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2016-02-17 15:11:13 -0200

    implementacao 17-02-2016

--- src/model/myapp/email.php
+++ src/model/myapp/email.php
@@ -32,7 +32,7 @@ function enviarEmailNovaSenha($nome, $email,$senha){
         $emp->getById(EMPRESA);    
         $mensagem = "Sr(a). $nome, sua nova senha para acesso é:<strong>$senha</strong>";
         $tplEmail = new Template("view/padrao/email.html");
-        $tplEmail -> ASSINATURA = str_replace("#url#",URL,str_replace("#nome#",$_SESSION['zurc.userNome'],str_replace("#logomarca#",$emp->logomarca,Email::ASSINATURA)));        
+        $tplEmail -> ASSINATURA = str_replace("#url#",URL,str_replace("#nome#","Condomínio",str_replace("#logomarca#",$emp->logomarca,Email::ASSINATURA)));        
         $tplEmail -> MENSAGEM = $mensagem;        
         return $this -> mail_html($email, REMETENTE, "Condomínio - Nova Senha", $tplEmail -> showString());
 }
